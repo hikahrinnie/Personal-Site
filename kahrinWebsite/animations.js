e6a4589c7342dd3de23232a2a4dfbc47af5f20ef
Kahrin Huggins <kahrinhuggins@gmail.com>
2020-11-02 18:00:34 -0500

    html, css and js files.
    Includes jQuery implementations +resume

---
+++ kahrinWebsite/animations.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('#about1').click(function () {
+    $('#slideBox1').slideToggle()
+  })
+})
+
+$(document).ready(function () {
+  $('#about2').click(function () {
+    $('#slideBox2').slideToggle()
+
+  })
+})
+
+$(document).ready(function () {
+  $('#about3').click(function () {
+    $('#slideBox3').slideToggle()
+
+  })
+})
